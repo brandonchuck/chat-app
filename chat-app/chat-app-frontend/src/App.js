fd441a104f8c3b6a3d50aff9c61144c46c7bec5e
Brandon Chuck <blc42310@gmail.com>
2022-02-21 22:10:45 -0800

    Installed tailwindcss for styling

--- chat-app/chat-app-frontend/src/App.js
+++ chat-app/chat-app-frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <div className="d-flex border-solid border-4 border-sky-500">
+        <p className=""> tailwindcss</p>
+        <button className="border-solid border-4 bg-slate-300 rounded border-sky-500 hover:border-dotted">
+          button
+        </button>
+        <div>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
